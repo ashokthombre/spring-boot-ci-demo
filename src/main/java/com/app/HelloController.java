b3943d3d0930eecf99c405233b8dbc35b83e8799
Ashok Thombre <ashok.marketyard@gmail.com>
2026-04-09 00:24:13 +0530

    Updated controller

--- src/main/java/com/app/HelloController.java
+++ src/main/java/com/app/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
 
     @GetMapping
      public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Hello, World!........!");
+        return ResponseEntity.ok("Hello, World added code.....");
     }
 }
